4141c5beac4c69f178344aa54a6c89b432703b8c
Grant Houston <grant.houston44@hotmail.com>
2019-12-13 14:57:39 +0000

    adding new list item tested

--- js/app.js
+++ js/app.js
@@ -2,23 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
   const newItemForm = document.querySelector('#new-item-form');
   newItemForm.addEventListener('submit', handleFormSubmit);
 
-  const deleteAllButton = document.querySelector('#delete-all');
-  deleteAllButton.addEventListener('click', handleDeleteAllButton);
-
-  const deleteItemButton = deleteItemButton.addEventListener('click', handleDeleteItemButton);
+  // const deleteAllButton = document.querySelector('#delete-all');
+  // deleteAllButton.addEventListener('click', handleDeleteAllButton);
+  //
+  // const deleteItemButton = deleteItemButton.addEventListener('click', handleDeleteItemButton);
 
 });
 
 const handleFormSubmit = function(event){
   event.preventDefault();
 
-  const vapourListItem = createVapourListItem(this);
+  const vapourListItem = createVapourListItem(event.target);
   const vapourList = document.querySelector('#vapour-list');
   vapourList.appendChild(vapourListItem);
 
-  this.reset()
+  event.target.reset()
   }
 
+  const createVapourListItem = function (form){
+    const vapourListItem = document.createElement('li');
+    vapourListItem.classList.add('vapour-list-item');
 
-  const newListItem = document.createElement('li');
-  newListItem.textContent = `Title: ${this.title.value}, Publisher: ${this.publisher.value}, Platform: ${this.platform.value}`
+    vapourListItem.textContent = `Title: ${this.title.value}, Publisher: ${this.publisher.value}, Platform: ${this.platform.value}`
+    return vapourListItem;
+  }
