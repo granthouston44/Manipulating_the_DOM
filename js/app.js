813efea359eb8dc9e00c151cb16f223451ab379b
Grant Houston <grant.houston44@hotmail.com>
2019-12-13 14:37:17 +0000

    handle new item form submit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
-document.addEventListener('DOMContentLoaded', () {
-  console.log('JavaScript loaded');
+document.addEventListener('DOMContentLoaded', () => {
+  const newItemForm = document.querySelector('#new-item-form');
+  newItemForm.addEventListener('submit', handleFormSubmit);
 
-const formElement = document.querySelector('#new-item-form');
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllButton);
 
-
-
-
-
+  const deleteItemButton = deleteItemButton.addEventListener('click', handleDeleteItemButton);
 
 });
+
+const handleFormSubmit = function(event){
+  event.preventDefault();
+
+  const vapourListItem = createVapourListItem(this);
+  const vapourList = document.querySelector('#vapour-list');
+  vapourList.appendChild(vapourListItem);
+
+  this.reset()
+  }
+
+
+  const newListItem = document.createElement('li');
+  newListItem.textContent = `Title: ${this.title.value}, Publisher: ${this.publisher.value}, Platform: ${this.platform.value}`
