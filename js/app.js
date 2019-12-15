d02a2f11022b080454d670f2671e9be01685e644
Grant Houston <grant.houston44@hotmail.com>
2019-12-15 17:48:38 +0000

    added delete specific item button

--- js/app.js
+++ js/app.js
@@ -5,8 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const deleteAllButton = document.querySelector('#button-all');
   deleteAllButton.addEventListener('click', handleDeleteAllButton);
 
-  // const deleteItemButton = deleteItemButton.addEventListener('click', handleDeleteItemButton);
-
 });
 
 const handleFormSubmit = function(event){
@@ -17,28 +15,40 @@ const handleFormSubmit = function(event){
   vapourList.appendChild(vapourListItem);
 
   event.target.reset()
-  }
+}
 
-  const createVapourListItem = function (form){
-    const vapourListItem = document.createElement('li');
-    vapourListItem.classList.add('vapour-list-item');
+const createVapourListItem = function (form){
+  const vapourListItem = document.createElement('li');
+  vapourListItem.classList.add('vapour-list-item');
 
-    const title = document.createElement('h2');
-    title.textContent = form.title.value;
-    vapourListItem.appendChild(title);
+  const title = document.createElement('h2');
+  title.textContent = form.title.value;
+  vapourListItem.appendChild(title);
 
-    const publisher = document.createElement('h3');
-    publisher.textContent = form.title.value;
-    vapourListItem.appendChild(publisher);
+  const publisher = document.createElement('h3');
+  publisher.textContent = form.publisher.value;
+  vapourListItem.appendChild(publisher);
 
-    const platform = document.createElement('p');
-    platform.textContent = form.title.value;
-    vapourListItem.appendChild(platform);
+  const platform = document.createElement('p');
+  platform.textContent = form.platform.value;
+  vapourListItem.appendChild(platform);
 
-    return vapourListItem;
-  }
+  const deleteItem = document.createElement('button');
+  deleteItem.innerHTML = "Delete Item"
+  deleteItem.id = "button-item";
+  vapourListItem.appendChild(deleteItem);
+  deleteItem.addEventListener('click', handleDeleteItemButton);
 
-  const handleDeleteAllButton = function (event){
-    const vapourList = document.querySelector('#vapour-list');
-    vapourList.innerHTML = '';
-  }
+
+  return vapourListItem;
+}
+
+const handleDeleteAllButton = function (event){
+  const vapourList = document.querySelector('#vapour-list');
+  vapourList.innerHTML = '';
+}
+
+const handleDeleteItemButton = function (event){
+  // const vapourList = document.getElementById('.vapour-list-item');
+  this.parentNode.parentNode.removeChild(this.parentNode);
+}
